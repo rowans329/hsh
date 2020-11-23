d702fc0b8f142d3675efd449703ef613cdcfb276
rowan <stephenson.rowan@icloud.com>
2020-11-23 12:58:52 -0500

    Add unit tests for Blake2Hasher

--- src/blake2/mod.rs
+++ src/blake2/mod.rs
@@ -15,3 +15,44 @@ impl Hasher for Blake2Hasher {
         hasher.finalize().to_vec()
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const HELLO_WORLD_HASH_BYTES: [u8; 64] = [
+        162, 118, 77, 19, 58, 22, 129, 107, 88, 71, 167, 55, 167, 134, 242, 236, 228, 193, 72, 9,
+        92, 95, 170, 115, 226, 75, 76, 197, 214, 102, 195, 228, 94, 194, 113, 80, 78, 20, 220, 97,
+        39, 221, 252, 228, 225, 68, 251, 35, 185, 26, 111, 123, 4, 181, 61, 105, 85, 2, 41, 7, 34,
+        149, 59, 15,
+    ];
+
+    const HELLO_WORLD_HASH_HEX: &str = "a2764d133a16816b5847a737a786f2ece4c148095c5faa73e24b4cc5d666c3e45ec271504e14dc6127ddfce4e144fb23b91a6f7b04b53d695502290722953b0f";
+
+    #[test]
+    fn blake2_hash_bytes_test() {
+        let bytes = b"Hello, world!";
+        let hash = Blake2Hasher.hash((), bytes);
+        assert_eq!(HELLO_WORLD_HASH_BYTES.to_vec(), hash);
+    }
+
+    #[test]
+    fn blake2_hash_string_test() {
+        let string = "Hello, world!";
+        let hash = Blake2Hasher.hash_str((), string);
+        assert_eq!(HELLO_WORLD_HASH_BYTES.to_vec(), hash);
+    }
+
+    #[test]
+    fn blake2_hash_bytes_hex_test() {
+        let bytes = b"Hello, world!";
+        let hash = Blake2Hasher.hash_hex((), bytes);
+        assert_eq!(HELLO_WORLD_HASH_HEX, hash);
+    }
+    #[test]
+    fn blake2_hash_string_hex_test() {
+        let string = "Hello, world!";
+        let hash = Blake2Hasher.hash_str_hex((), string);
+        assert_eq!(HELLO_WORLD_HASH_HEX, hash);
+    }
+}

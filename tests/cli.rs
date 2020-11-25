687215fb15a527ff1112ea356dcf6516865f4e27
rowan <stephenson.rowan@icloud.com>
2020-11-24 19:59:56 -0500

    Add integration tests for bcrypt

--- tests/cli.rs
+++ tests/cli.rs
@@ -48,7 +48,7 @@ fn missing_args() -> Result<(), Box<dyn Error>> {
 fn invalid_hash_function() -> Result<(), Box<dyn Error>> {
     let cmd = setup(vec!["Hello, world!", "-f", "foo"])?;
 
-    assert_errors(cmd, vec!["invalid hash function: `foo`"]);
+    assert_errors(cmd, vec!["invalid hash function `foo`"]);
 
     Ok(())
 }
@@ -67,6 +67,60 @@ fn sha1_bytes_test() -> Result<(), Box<dyn Error>> {
     let cmd = setup(vec!["Hello, world!", "-bf", "sha1"])?;
 
     assert_success(cmd, vec!["[148, 58, 112, 45, 6, 243, 69, 153, 174, 225, 248, 218, 142, 249, 247, 41, 96, 49, 214, 153]"]);
+
+    Ok(())
+}
+
+#[test]
+fn missing_bcrypt_inputs() -> Result<(), Box<dyn Error>> {
+    let cmd = setup(vec!["Hello, world!", "-f", "bcrypt"])?;
+
+    assert_errors(cmd, vec!["The following required arguments were not provided:", "--cost <cost>"]);
+
+    Ok(())
+}
+
+#[test]
+fn missing_bcrypt_salt() -> Result<(), Box<dyn Error>> {
+    let cmd = setup(vec!["Hello, world!", "-f", "bcrypt", "-c", "1"])?;
+
+    assert_errors(cmd, vec!["The following required arguments were not provided:", "--salt <salt>"]);
+
+    Ok(())
+}
+
+#[test]
+fn bcrypt_invalid_salt_hex_odd_digits() -> Result<(), Box<dyn Error>> {
+    let cmd = setup(vec!["Hello, world!", "-f", "bcrypt", "-c", "1", "-s", "1a54e"])?;
+
+    assert_errors(cmd, vec!["invalid salt hex -- Odd number of digits"]);
+
+    Ok(())
+}
+
+#[test]
+fn bcrypt_invalid_salt_incorrect_length_short() -> Result<(), Box<dyn Error>> {
+    let cmd = setup(vec!["Hello, world!", "-f", "bcrypt", "-c", "1", "-s", "000102030405"])?;
+
+    assert_errors(cmd, vec!["invalid salt -- incorrect salt length (should be 16 bytes, found 6)"]);
+
+    Ok(())
+}
+
+#[test]
+fn bcrypt_invalid_salt_incorrect_length_long() -> Result<(), Box<dyn Error>> {
+    let cmd = setup(vec!["Hello, world!", "-f", "bcrypt", "-c", "1", "-s", "000102030405060708090a0b0c0d0e0f111213141516"])?;
+
+    assert_errors(cmd, vec!["invalid salt -- incorrect salt length (should be 16 bytes, found 22)"]);
+
+    Ok(())
+}
+
+#[test]
+fn bcrypt_hex_test() -> Result<(), Box<dyn Error>> {
+    let cmd = setup(vec!["Hello, world!", "-f", "bcrypt", "-c", "1", "-s", "000102030405060708090a0b0c0d0e0f"])?;
+
+    assert_success(cmd, vec!["0e96480b69ed820d168679047ec1625d334fc586df53823e"]);
 
     Ok(())
 }

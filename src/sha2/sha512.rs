70026d9f59d0879f5b0e8200bbf6aacd760dac72
rowan <stephenson.rowan@icloud.com>
2020-11-27 11:22:11 -0500

    :white_check_mark: Add unit tests for sha2

--- src/sha2/sha512.rs
+++ src/sha2/sha512.rs
@@ -16,3 +16,41 @@ impl Hasher for Sha512Hasher {
         HashOutput::new(hasher.finalize())
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use proptest::prelude::*;
+
+    #[test]
+    fn test_sha512_hash_password() {
+        let password = "password";
+
+        let hash = Sha512Hasher.hash_str((), password);
+
+        assert_eq!("b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86", hash.as_hex());
+    }
+
+    #[test]
+    fn test_sha512_hash_bytes() {
+        let bytes = b"password";
+
+        let hash = Sha512Hasher.hash((), bytes);
+
+        assert_eq!("b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86", hash.as_hex());
+    }
+
+    proptest! {
+        #[test]
+        fn fuzz_sha512_hash_does_not_panic(pass in ".*") {
+            let _ = Sha512Hasher.hash_str((), &pass);
+        }
+
+        #[test]
+        fn fuzz_sha512_hash_bytes_does_not_panic(
+            bytes in proptest::collection::vec(any::<u8>(), 0..1000)
+        ) {
+            let _ = Sha512Hasher.hash((), &bytes);
+        }
+    }
+}

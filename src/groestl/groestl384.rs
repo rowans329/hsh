da17cbf133a1d45ee2da147a15a25f7c1500234c
rowan <stephenson.rowan@icloud.com>
2020-11-27 11:20:01 -0500

    :white_check_mark: Add unit tests for groestl

--- src/groestl/groestl384.rs
+++ src/groestl/groestl384.rs
@@ -16,3 +16,41 @@ impl Hasher for Groestl384Hasher {
         HashOutput::new(hasher.finalize())
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use proptest::prelude::*;
+
+    #[test]
+    fn test_groestl384_hash_password() {
+        let password = "password";
+
+        let hash = Groestl384Hasher.hash_str((), password);
+
+        assert_eq!("689cc12d7c232ab2503cc596b4973ea1e7b75361cfbbfd947c92ef02b65b5b1ca71c4391d3c50f3d092481d6ba459b13", hash.as_hex());
+    }
+
+    #[test]
+    fn test_groestl384_hash_bytes() {
+        let bytes = b"password";
+
+        let hash = Groestl384Hasher.hash((), bytes);
+
+        assert_eq!("689cc12d7c232ab2503cc596b4973ea1e7b75361cfbbfd947c92ef02b65b5b1ca71c4391d3c50f3d092481d6ba459b13", hash.as_hex());
+    }
+
+    proptest! {
+        #[test]
+        fn fuzz_groestl384_hash_does_not_panic(pass in ".*") {
+            let _ = Groestl384Hasher.hash_str((), &pass);
+        }
+
+        #[test]
+        fn fuzz_groestl384_hash_bytes_does_not_panic(
+            bytes in proptest::collection::vec(any::<u8>(), 0..1000)
+        ) {
+            let _ = Groestl384Hasher.hash((), &bytes);
+        }
+    }
+}
--- src/groestl/groestl512.rs
+++ src/groestl/groestl512.rs
@@ -16,3 +16,41 @@ impl Hasher for Groestl512Hasher {
         HashOutput::new(hasher.finalize())
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use proptest::prelude::*;
+
+    #[test]
+    fn test_groestl512_hash_password() {
+        let password = "password";
+
+        let hash = Groestl512Hasher.hash_str((), password);
+
+        assert_eq!("ced825c4699fc51ff75c3031357994e7abaa94b38a5e037cfd075021a9dba00fc54c586eb0b7cb01fede27f6b61dde292f5a7e8ab42ccbd11bef8b538119750d", hash.as_hex());
+    }
+
+    #[test]
+    fn test_groestl512_hash_bytes() {
+        let bytes = b"password";
+
+        let hash = Groestl512Hasher.hash((), bytes);
+
+        assert_eq!("ced825c4699fc51ff75c3031357994e7abaa94b38a5e037cfd075021a9dba00fc54c586eb0b7cb01fede27f6b61dde292f5a7e8ab42ccbd11bef8b538119750d", hash.as_hex());
+    }
+
+    proptest! {
+        #[test]
+        fn fuzz_groestl512_hash_does_not_panic(pass in ".*") {
+            let _ = Groestl512Hasher.hash_str((), &pass);
+        }
+
+        #[test]
+        fn fuzz_groestl512_hash_bytes_does_not_panic(
+            bytes in proptest::collection::vec(any::<u8>(), 0..1000)
+        ) {
+            let _ = Groestl512Hasher.hash((), &bytes);
+        }
+    }
+}

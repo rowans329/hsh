6e8a6f49fc328d922ff23d3b3ade44d674694837
rowan <stephenson.rowan@icloud.com>
2020-11-27 11:23:08 -0500

    :white_check_mark: Add unit tests for error

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,12 @@
 // Std imports
+use std::cmp::PartialEq;
 use std::error::Error;
 use std::fmt::{self, Debug, Display};
 
 // External imports
 use hex::FromHexError;
 
+#[derive(PartialEq)]
 pub enum HshErr {
     InvalidHashFunction(String),
     InvalidSalt(String),
@@ -30,3 +32,80 @@ impl Display for HshErr {
 impl Error for HshErr {}
 
 pub type HshResult<T> = Result<T, HshErr>;
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_hsh_err_debug_invalid_function() {
+        let err = HshErr::InvalidHashFunction(String::from("foobar"));
+
+        let err_msg = format!("{:?}", err);
+
+        assert_eq!(
+            format!("invalid hash function `foobar`"),
+            err_msg,
+        )
+    }
+
+    #[test]
+    fn test_hsh_err_debug_invalid_salt() {
+        let err = HshErr::InvalidSalt(String::from("foobar"));
+
+        let err_msg = format!("{:?}", err);
+
+        assert_eq!(
+            format!("invalid salt -- foobar"),
+            err_msg,
+        )
+    }
+
+    #[test]
+    fn test_hsh_err_debug_invalid_salt_hex() {
+        let err = HshErr::InvalidSaltHex(FromHexError::OddLength);
+
+        let err_msg = format!("{:?}", err);
+
+        assert_eq!(
+            format!("invalid salt hex -- Odd number of digits"),
+            err_msg,
+        )
+    }
+
+    #[test]
+    fn test_hsh_err_display_invalid_function() {
+        let err = HshErr::InvalidHashFunction(String::from("foobar"));
+
+        let err_msg = format!("{}", err);
+
+        assert_eq!(
+            format!("invalid hash function `foobar`"),
+            err_msg,
+        )
+    }
+
+    #[test]
+    fn test_hsh_err_display_invalid_salt() {
+        let err = HshErr::InvalidSalt(String::from("foobar"));
+
+        let err_msg = format!("{}", err);
+
+        assert_eq!(
+            format!("invalid salt -- foobar"),
+            err_msg,
+        )
+    }
+
+    #[test]
+    fn test_hsh_err_display_invalid_salt_hex() {
+        let err = HshErr::InvalidSaltHex(FromHexError::OddLength);
+
+        let err_msg = format!("{}", err);
+
+        assert_eq!(
+            format!("invalid salt hex -- Odd number of digits"),
+            err_msg,
+        )
+    }
+}

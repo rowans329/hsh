3fae78a8afc0596fa5607f2a19169d46d3bb3c58
rowan <stephenson.rowan@icloud.com>
2020-11-27 11:19:31 -0500

    :white_check_mark: Add unit tests for blake2

--- src/blake2/mod.rs
+++ src/blake2/mod.rs
@@ -20,40 +20,37 @@ impl Hasher for Blake2Hasher {
 #[cfg(test)]
 mod test {
     use super::*;
-
-    const HELLO_WORLD_HASH_BYTES: [u8; 64] = [
-        162, 118, 77, 19, 58, 22, 129, 107, 88, 71, 167, 55, 167, 134, 242, 236, 228, 193, 72, 9,
-        92, 95, 170, 115, 226, 75, 76, 197, 214, 102, 195, 228, 94, 194, 113, 80, 78, 20, 220, 97,
-        39, 221, 252, 228, 225, 68, 251, 35, 185, 26, 111, 123, 4, 181, 61, 105, 85, 2, 41, 7, 34,
-        149, 59, 15,
-    ];
-
-    const HELLO_WORLD_HASH_HEX: &str = "a2764d133a16816b5847a737a786f2ece4c148095c5faa73e24b4cc5d666c3e45ec271504e14dc6127ddfce4e144fb23b91a6f7b04b53d695502290722953b0f";
+    use proptest::prelude::*;
 
     #[test]
-    fn blake2_hash_bytes_test() {
-        let bytes = b"Hello, world!";
+    fn test_blake2_hash_password() {
+        let password = "password";
+
+        let hash = Blake2Hasher.hash_str((), password);
+
+        assert_eq!("7c863950ac93c93692995e4732ce1e1466ad74a775352ffbaaf2a4a4ce9b549d0b414a1f3150452be6c7c72c694a7cb46f76452917298d33e67611f0a42addb8", hash.as_hex());
+    }
+
+    #[test]
+    fn test_blake2_hash_bytes() {
+        let bytes = b"password";
+
         let hash = Blake2Hasher.hash((), bytes);
-        assert_eq!(HELLO_WORLD_HASH_BYTES.to_vec(), hash.as_bytes());
+
+        assert_eq!("7c863950ac93c93692995e4732ce1e1466ad74a775352ffbaaf2a4a4ce9b549d0b414a1f3150452be6c7c72c694a7cb46f76452917298d33e67611f0a42addb8", hash.as_hex());
     }
 
-    #[test]
-    fn blake2_hash_string_test() {
-        let string = "Hello, world!";
-        let hash = Blake2Hasher.hash_str((), string);
-        assert_eq!(HELLO_WORLD_HASH_BYTES.to_vec(), hash.as_bytes());
-    }
+    proptest! {
+        #[test]
+        fn fuzz_blake2_hash_does_not_panic(pass in ".*") {
+            let _ = Blake2Hasher.hash_str((), &pass);
+        }
 
-    #[test]
-    fn blake2_hash_bytes_hex_test() {
-        let bytes = b"Hello, world!";
-        let hash = Blake2Hasher.hash((), bytes);
-        assert_eq!(HELLO_WORLD_HASH_HEX, hash.as_hex());
-    }
-    #[test]
-    fn blake2_hash_string_hex_test() {
-        let string = "Hello, world!";
-        let hash = Blake2Hasher.hash_str((), string);
-        assert_eq!(HELLO_WORLD_HASH_HEX, hash.as_hex());
+        #[test]
+        fn fuzz_blake2_hash_bytes_does_not_panic(
+            bytes in proptest::collection::vec(any::<u8>(), 0..1000)
+        ) {
+            let _ = Blake2Hasher.hash((), &bytes);
+        }
     }
 }

e59a1b2bbf36e30bb64378812146277402b0fa14
rowan <stephenson.rowan@icloud.com>
2020-11-27 11:22:51 -0500

    :white_check_mark: Add unit tests for whirlpool

--- src/whirlpool/mod.rs
+++ src/whirlpool/mod.rs
@@ -16,3 +16,41 @@ impl Hasher for WhirlpoolHasher {
         HashOutput::new(hasher.finalize())
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use proptest::prelude::*;
+
+    #[test]
+    fn test_whirlpool_hash_password() {
+        let password = "password";
+
+        let hash = WhirlpoolHasher.hash_str((), password);
+
+        assert_eq!("74dfc2b27acfa364da55f93a5caee29ccad3557247eda238831b3e9bd931b01d77fe994e4f12b9d4cfa92a124461d2065197d8cf7f33fc88566da2db2a4d6eae", hash.as_hex());
+    }
+
+    #[test]
+    fn test_whirlpool_hash_bytes() {
+        let bytes = b"password";
+
+        let hash = WhirlpoolHasher.hash((), bytes);
+
+        assert_eq!("74dfc2b27acfa364da55f93a5caee29ccad3557247eda238831b3e9bd931b01d77fe994e4f12b9d4cfa92a124461d2065197d8cf7f33fc88566da2db2a4d6eae", hash.as_hex());
+    }
+
+    proptest! {
+        #[test]
+        fn fuzz_whirlpool_hash_does_not_panic(pass in ".*") {
+            let _ = WhirlpoolHasher.hash_str((), &pass);
+        }
+
+        #[test]
+        fn fuzz_whirlpool_hash_bytes_does_not_panic(
+            bytes in proptest::collection::vec(any::<u8>(), 0..1000)
+        ) {
+            let _ = WhirlpoolHasher.hash((), &bytes);
+        }
+    }
+}

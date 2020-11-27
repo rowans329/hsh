a2006dfb265393678d7cf26a4fddcb609c4f1b26
rowan <stephenson.rowan@icloud.com>
2020-11-27 11:21:22 -0500

    :white_check_mark: Add unit tests for md4

--- src/md4/mod.rs
+++ src/md4/mod.rs
@@ -16,3 +16,41 @@ impl Hasher for Md4Hasher {
         HashOutput::new(hasher.finalize())
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use proptest::prelude::*;
+
+    #[test]
+    fn test_md4_hash_password() {
+        let password = "password";
+
+        let hash = Md4Hasher.hash_str((), password);
+
+        assert_eq!("8a9d093f14f8701df17732b2bb182c74", hash.as_hex());
+    }
+
+    #[test]
+    fn test_md4_hash_bytes() {
+        let bytes = b"password";
+
+        let hash = Md4Hasher.hash((), bytes);
+
+        assert_eq!("8a9d093f14f8701df17732b2bb182c74", hash.as_hex());
+    }
+
+    proptest! {
+        #[test]
+        fn fuzz_md4_hash_does_not_panic(pass in ".*") {
+            let _ = Md4Hasher.hash_str((), &pass);
+        }
+
+        #[test]
+        fn fuzz_md4_hash_bytes_does_not_panic(
+            bytes in proptest::collection::vec(any::<u8>(), 0..1000)
+        ) {
+            let _ = Md4Hasher.hash((), &bytes);
+        }
+    }
+}

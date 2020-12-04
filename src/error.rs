d9854b592e2b5dd8b9641df29ad28ec7fd8509da
rowan <stephenson.rowan@icloud.com>
2020-12-04 13:57:47 -0500

    Add tests for error methods and functionality

--- src/error.rs
+++ src/error.rs
@@ -86,3 +86,104 @@ impl<T> UnwrapOrExit<T> for HshResult<T> {
 pub trait UnwrapOrExit<T> {
     fn unwrap_or_exit(self) -> T;
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn hsh_err_display_salt_from_str_error() {
+        let err = HshErr::SaltFromStrError(SaltFromStrError::BlankStr);
+        let msg = format!("{}", err);
+        assert_eq!("error parsing salt: salt cannot be blank", &msg);
+    }
+
+    #[test]
+    fn hsh_err_display_unsuported_str_length() {
+        let err = HshErr::UnsuportedStrLength(String::from("input string for bcrypt hash function must be between 0 and 72 bytes"));
+        let msg = format!("{}", err);
+        assert_eq!("unsuported string length: input string for bcrypt hash function must be between 0 and 72 bytes", &msg);
+    }
+
+    #[test]
+    fn hsh_err_exit_code_salt_from_str_error() {
+        let err = HshErr::SaltFromStrError(SaltFromStrError::BlankStr);
+        let code = err.exitcode();
+        assert_eq!(65i64, code);
+    }
+
+    #[test]
+    fn hsh_err_exit_code_unsuported_str_length() {
+        let err = HshErr::UnsuportedStrLength(String::from("input string for bcrypt hash function must be between 0 and 72 bytes"));
+        let code = err.exitcode();
+        assert_eq!(65i64, code);
+    }
+
+    #[test]
+    fn hsh_err_from_salt_from_str_error() {
+        let err = HshErr::from(SaltFromStrError::BlankStr);
+        assert_eq!(HshErr::SaltFromStrError(SaltFromStrError::BlankStr));
+    }
+
+    #[test]
+    fn salt_from_str_error_display_blank_str() {
+        let err = SaltFromStrError::BlankStr;
+        let msg = format!("{}", err);
+        assert_eq!("salt cannot be blank", &msg);
+    }
+
+    #[test]
+    fn salt_from_str_error_display_incorrect_length() {
+        let err = SaltFromStrError::IncorrectLength(16, 12);
+        let msg = format!("{}", err);
+        assert_eq!("incorrect salt length (expected 16 bytes, found 12)", &msg);
+    }
+
+    #[test]
+    fn salt_from_str_error_display_invalid_base64_character() {
+        let err = SaltFromStrError::InvalidBase64Character('~', 4);
+        let msg = format!("{}", err);
+        assert_eq!("invalid character '~' in base64 string at index 4", &msg);
+    }
+
+    #[test]
+    fn salt_from_str_error_display_invalid_base64_length() {
+        let err = SaltFromStrError::InvalidBase64Length;
+        let msg = format!("{}", err);
+        assert_eq!("length of base64 string must be divisible by 4", &msg);
+    }
+
+    #[test]
+    fn salt_from_str_error_display_invalid_byte() {
+        let err = SaltFromStrError::InvalidByte("-1", 2);
+        let msg = format!("{}", err);
+        assert_eq!("byte input contains invalid byte \"-1\" at array index 2", &msg);
+    }
+
+    #[test]
+    fn salt_from_str_error_display_invalid_byte_format() {
+        let err = SaltFromStrError::InvalidByteFormat;
+        let msg = format!("{}", err);
+        assert_eq!("byte input was incorrectly formatted (string should begin and end with '[' and ']' and contain a comma-separated list of values)", &msg);
+    }
+
+    #[test]
+    fn salt_from_str_error_display_invalid_hex_character() {
+        let err = SaltFromStrError::InvalidHexCharacter('~', 4);
+        let msg = format!("{}", err);
+        assert_eq!("invalid character '~' in hex string at index 4", &msg);
+    }
+
+    #[test]
+    fn salt_from_str_error_display_invalid_hex_length() {
+        let err = SaltFromStrError::InvalidHexLength;
+        let msg = format!("{}", err);
+        assert_eq!("hex must be of even length", &msg);
+    }
+
+    #[test]
+    fn test_hsh_result_unwrap_or_exit_ok() {
+        let res: HshResult<i64> = Ok(100);
+        assert_eq!(100, res.unwrap_or_exit());
+    }
+}
